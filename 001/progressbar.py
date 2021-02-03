113a9c5b85ca187708ea5985c28ea8bc618e9b44
chilusoft <mukukachilu@gmail.com>
2021-02-03 20:09:48 +0200

    significant improvements

--- 001/progressbar.py
+++ 001/progressbar.py
@@ -1,4 +1,4 @@
-import math, time, subprocess, sys
+import sys, math, time, subprocess, random
 
 
 def bar(num):
@@ -6,19 +6,24 @@ def bar(num):
 	t1 = time.time()
 
 	for x in range(0, num):
-		print('initailising progress bar...')
-		print(str(x) + '/' + str(num) + '| ' + '='*x + '=> ' + str(math.floor(x/num*100)) + '% done...')
-		time.sleep(0.4)
+		if x == 0:
+			print('initailising progress bar...')
+			time.sleep(1)
+		c = ['/','-','\\','|',]
+
+		print('Running Progress bar ' + c[x % 4])
+		print(str(x) + '/' + str(num) + '| ' + '=' * x + '=> ' + str(math.floor((x/num) * 100)) + '% done...')
+		time.sleep(1/4)
 		if sys.platform == 'win32':
 			subprocess.call('cls', shell=True) #windows
-		elif sys.platform =='linux2' or'darwin':
+		elif sys.platform =='linux2' or 'darwin':
 			subprocess.call('clear', shell=True) #unix
 		else:
 			print('unrecognised OS environment')
 			break
 
 
-		if x == num -1:
+		if x == num - 1:
 			print(str(num) + '/'+ str(num) + '='*num + ' 100% done!!!')
 			print('loop ran for ' + str(math.floor(time.time() -t1)) + ' seconds ....')
 
