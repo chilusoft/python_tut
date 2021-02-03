ca21a24cb31cf3ee476a54102dd3d69389329216
chilusoft <mukukachilu@gmail.com>
2021-02-03 19:34:29 +0200

    added support for windows

--- 001/progressbar.py
+++ 001/progressbar.py
@@ -1,4 +1,4 @@
-import math, time, subprocess
+import math, time, subprocess, sys
 
 
 def bar(num):
@@ -9,12 +9,19 @@ def bar(num):
 		print('initailising progress bar...')
 		print(str(x) + '/' + str(num) + '| ' + '='*x + '=> ' + str(math.floor(x/num*100)) + '% done...')
 		time.sleep(0.4)
-		subprocess.call('clear', shell=True) #unix
+		if sys.platform == 'win32':
+			subprocess.call('cls', shell=True) #windows
+		elif sys.platform =='linux2' or'darwin':
+			subprocess.call('clear', shell=True) #unix
+		else:
+			print('unrecognised OS environment')
+			break
+
 
 		if x == num -1:
 			print(str(num) + '/'+ str(num) + '='*num + ' 100% done!!!')
-			print('loop ran for ' + str(math.floor(time.time() -t1)) + 'seconds ....')
+			print('loop ran for ' + str(math.floor(time.time() -t1)) + ' seconds ....')
 
-n = int(input('enter the range for the progress bar'))
+n = int(input('enter the range for the progress bar: '))
 
 bar(n)
